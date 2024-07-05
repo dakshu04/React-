9ab4f7440898ef4320eb6fd83c619eecafeea753
Daksh Purohit <dakshgrows@gmail.com>
2024-07-05 18:29:45 +0530

    dark-light mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,20 +9,43 @@ export default function TextForm(props) {
         setText(newText); 
     }
 
+    const handleLoClick = () => {
+        // console.log("Upper Case Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText); 
+    }
+
+    const handleClClick = () => {
+        let newText = "";
+        setText(newText); 
+    }
+
     const handleOnChange = (event) => {
         // console.log("On Change");
         setText(event.target.value )
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     return (
-        <div>
-        <h1>{props.heading} </h1>
+        <>
+        <div className="container">
+        <h2>{props.heading} </h2>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Case</button>
-</div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
+        <button className="btn btn-primary mx-2" onClick={handleClClick}>Clear Text</button>
+        </div>
+
+        <div className="container my-3">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length - 1} words and {text.length}</p>
+            <p>{ 0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
   );
 }
